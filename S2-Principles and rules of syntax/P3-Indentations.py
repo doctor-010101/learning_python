35d15248e89e18f06078c0325f27338926a139da
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-21 00:38:59 +0330

    The content of the third part of the second season was added

---
+++ S2-Principles and rules of syntax/P3-Indentations.py
@@ -0,0 +1,10 @@
+""""Indents are used to make the code readable, such as specifying commands belonging to a function."""
+
+# Example
+
+
+def sayHello():
+    print("Hi hi")
+
+
+# Full explanations about indents will come in the next chapters
