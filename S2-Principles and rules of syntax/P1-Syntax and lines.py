5ea6d3066ee85ed776386837ee31cd12d7da82d7
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-20 22:27:21 +0330

    The content of the first part of the second season was added

---
+++ S2-Principles and rules of syntax/P1-Syntax and lines.py
@@ -0,0 +1,21 @@
+# Syntax => 👇
+
+    # The syntax of a programming language is a set of rules and structures that determine how to write and combine programming code.
+    # These rules include how variables are defined, the use of operators, the definition of functions and classes, control structures and conditional statements, loops, and so on.
+    # Each programming language has its own specific syntax, based on which programming code must be written.
+    
+# Lines => 👇
+
+    # 1-Physical lines =>The lines that are numbered in the editor or IDE. For example=> 👈this is the seventh (7) line
+    # 2-Logical lines => The lines that the interpreter takes as lines. For example => print("hello");
+    
+    # Tips 👇
+    
+        # 1 => Sometimes a logical line consists of several physical lines and conversely.
+        # 2 => To separate logical lines ";" is used.
+        # 3 => "/" is used to break the line in long commands.
+        # 4 =>  Comments are not separated by "/".
+        # 5 => Comments cannot be written between logical lines.
+        # 6 => If there are values inside ( ) or [ ], you don't need "/" to separate them and break to the next line, and you can comment after them.
+        
+    
