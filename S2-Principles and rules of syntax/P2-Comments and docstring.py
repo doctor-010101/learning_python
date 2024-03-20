3262b592418d9c1630e4a22c6c2b116ebfd3dbf1
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-20 23:38:52 +0330

    The content of the second part of the second season was added

---
+++ S2-Principles and rules of syntax/P2-Comments and docstring.py
@@ -0,0 +1,16 @@
+# 1 => "#" is used for comments in Python.
+# 
+    # - Benefits of commenting 👇
+    
+        # - If someone or I later want to read the codes to understand what each code does.
+        # - In order for the interpreter to temporarily have nothing to do with some of my codes.
+# 
+# """Docstring""" use forIt is for explanations about the work of functions and classes, etc.
+# 
+# The difference between comments and docstrings 👇
+
+    # - Comments are for programmers, but docstrings are for users use the code.
+    # - Interpreters always ignore comments, but if docstrings are included inside functions or classes, the interpreter does not ignore them, but if they are outside them, they are like comments.
+# 
+# Full explanations about docstrings will come in the next chapters.
+
