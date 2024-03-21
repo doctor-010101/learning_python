9e978314f8a4cb52fafe141e6795c805bc0870e1
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-22 03:06:12 +0330

    The content of the sixth part of the second season was added

---
+++ S2_Principles_and_rules_of_syntax/P6_identifiers.py
@@ -0,0 +1,18 @@
+# Identifiers are names given to variables, functions, classes, etc
+#
+# Python is case sensitive.
+#
+# The names of the variables should start with "_, upper case and lower case" and the snake_case method should be used to separate the words.
+#
+# There is no problem with numbers and letters after "_, upper case and lower case"
+#
+# You can't use #, $, %, spaces, etc
+
+_a = 10
+a21a_ = 20
+A = 30
+_12 = 40
+
+# Functions are named both by camelCase and snake_case methods
+#
+# Classes are named in the PascalCase.
