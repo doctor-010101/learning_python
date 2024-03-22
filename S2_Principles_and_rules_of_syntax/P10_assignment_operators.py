9d76503ae6c318546189ae0385f215fd22ae47ed
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-22 15:33:58 +0330

    The content of the tenth part of the second season was added

---
+++ S2_Principles_and_rules_of_syntax/P10_assignment_operators.py
@@ -0,0 +1,32 @@
+"""
+We have thirteen arithmetic operators which are divided into two types:
+    1 => Normal assignment by "=" 1
+    2 => arithmetic assignment(first it is calculated and then the result is assigned) 12
+"""
+
+x = 10  # The right side is assigned to the left side.
+
+x += 2  # Same as => x = x + 2, Result: 10 + 2 = 12 => x = 12
+
+x -= 2  # Same as => x = x - 2, Result: 10 - 2 = 8 => x = 8
+
+x *= 2  # Same as => x = x * 2, Result: 10 * 2 = 20 => x = 20
+
+x /= 2  # Same as => x = x / 2, Result: 10 / 2 = 5.0 => x = 5.0
+
+x %= 2  # Same as => x = x % 2, Result: 10 % 2 = 0 => x = 0
+
+x //= 2  # Same as => x = x // 2, Result: 10 // 2 = 5 => x = 5
+
+x **= 2  # Same as => x = x ** 2, Result: 10 ** 2 = 100 => x = 100
+
+x &= 2  # Same as => x = x & 2, Result: 10 & 2 = 100 => x = 100
+
+x |= 2  # Same as => x = x | 2, Result: 10 | 2 = 100 => x = 100
+
+x ^= 2  # Same as => x = x ^ 2, Result: 10 ^ 2 = 100 => x = 100
+
+x >>= 2  # Same as => x = x >> 2, Result: 10 >> 2 = 100 => x = 100
+
+x <<= 2  # Same as => x = x << 2, Result: 10 << 2 = 100 => x = 100
+
