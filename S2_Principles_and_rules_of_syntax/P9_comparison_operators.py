1c72aafb68cec24282c59d6dd05b61fb431b4297
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-22 14:33:26 +0330

    The content of the ninth part of the second season was added

---
+++ S2_Principles_and_rules_of_syntax/P9_comparison_operators.py
@@ -0,0 +1,11 @@
+# Comparison Operators => That are used for comparison and their result is either true or false.
+
+x = 4
+y = 5
+
+print(x == y)  # x equal y result => False
+print(x != y)  # x not equal y result => True
+print(x > y)  #  x greater than y result => False
+print(x < y)  # x less than y result => True
+print(x >= y)  # x greater than or equal to y result => False
+print(x <= y)  # x greater than or equal to y result => True
