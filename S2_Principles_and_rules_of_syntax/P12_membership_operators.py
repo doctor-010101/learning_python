d8ab7751683feef0f5593a13507cf04bcec8f10a
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-22 19:36:35 +0330

    The content of the twelfth part of the second season was added

---
+++ S2_Principles_and_rules_of_syntax/P12_membership_operators.py
@@ -0,0 +1,9 @@
+# Membership operators are used to check whether a value is in a set or not
+
+name = "python"
+# in
+print("p" in name)  # True
+print("z" in name)  # False
+# not in
+print("p" not in name)  # False
+print("z" not in name)  # True
