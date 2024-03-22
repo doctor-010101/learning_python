bb0ae8d351753a3d4ae9d29b2c5e35bbe6ccc5b7
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-22 03:59:31 +0330

    The content of the eightth part of the second season was added

---
+++ S2_Principles_and_rules_of_syntax/P.8_arithmetic_operators.py
@@ -0,0 +1,10 @@
+x = 20
+y = 7
+
+print(x + y)  # additional operator result => 27
+print(x - y)  # subtraction operator result => 13
+print(x * y)  # multiplication operator result => 140
+print(x / y)  # division operator result => 2.857142857142857
+print(x % y)  # modulus operator result => 6
+print(x**y)  # exponentiation operator result => 1280000000
+print(x // y)  # floor division operator result => 2
