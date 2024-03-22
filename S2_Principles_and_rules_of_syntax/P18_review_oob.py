08576f3a84c7e72189efc537827df93f42916757
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-23 01:26:49 +0330

    The content of the eighteenth part of the second season was added

---
+++ S2_Principles_and_rules_of_syntax/P18_review_oob.py
@@ -0,0 +1,15 @@
+"""
+
+Object-oriented programming is a programming paradigm that focuses on the concept of objects and the interactions between them.
+In object-oriented programming, data and operations related to a specific object are encapsulated in a unit (object), which can have different attributes (variables) and behaviors (methods).
+
+In Python, everything is an object, even numbers, strings, lists, functions, and so on.
+In object-oriented programming in Python, we can define classes that define common patterns among objects.
+Then, we can create instances of these classes, which contain specific data and behaviors.
+
+By using object-oriented programming in Python, you can write your code in a more organized and maintainable way.
+This paradigm allows you to easily work with objects and their relationships.
+
+👉 We will get to know this topic more fully in the next chapters 😍
+
+"""
