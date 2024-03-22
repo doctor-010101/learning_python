b62072c33657c6c0493a7e630f3d4c6bec91548f
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-23 02:18:20 +0330

    The content of the nineteenth part of the second season was added

---
+++ S2_Principles_and_rules_of_syntax/P19_pep8_pep20.py
@@ -0,0 +1,21 @@
+"""
+
+PEP 8 and PEP 20 are two important documents in the Python community.
+That provide guidelines for writing clean, readable, and consistent Python code. Let's break down each one:
+
+1. PEP 8 (Python Enhancement Proposal 8):
+   - PEP 8 is the official style guide for Python code.
+   - It was written by Guido van Rossum, Barry Warsaw, and Nick Coghlan.
+   - PEP 8 outlines the best practices for writing Python code to ensure readability and maintainability.
+   - It covers topics such as naming conventions, indentation, line length, imports, whitespace, comments, and more.
+   - Adhering to PEP 8 helps make your code more consistent and easier to understand for other developers.
+
+2. PEP 20 (Python Enhancement Proposal 20):
+   - PEP 20 is also known as "The Zen of Python."
+   - It was written by Tim Peters and serves as a set of guiding principles for writing Python code.
+   - PEP 20 emphasizes the importance of readability, simplicity, and clarity in Python code.
+   - It contains 19 aphorisms that reflect Python's design philosophy and encourage developers to write code that is elegant and easy to understand.
+   - Some key principles from PEP 20 include "Beautiful is better than ugly," "Simple is better than complex," "Readability counts," and "There should be one– and preferably only one –obvious way to do it."
+
+"""
+import this # If you run this line you can read pep20 in terminal.
