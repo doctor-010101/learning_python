d7e266cc86273d31c4caf66f00e19c7a3a906df1
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-22 23:32:11 +0330

    The content of the fifteenth part of the second season was added

---
+++ S2_Principles_and_rules_of_syntax/p15_walrus_operator.py
@@ -0,0 +1,7 @@
+"""
+The Walrus Operator is a new feature in Python 3.8 that is introduced using the := symbol.
+This operator allows you to assign a value to a variable and at the same time use that value as the output of the expression.
+This operator is very useful for coding in loops and various conditions.
+"""
+
+print((x := 10) > 20)
