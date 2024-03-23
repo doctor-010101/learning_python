1d2537256570a5ed66de8486b6a058073ad5f5e6
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-24 02:35:41 +0330

    The content of the first part of the third season was added

---
+++ S3_data_types_(level_one)/P1.data_types.py
@@ -0,0 +1,32 @@
+# Data types in Python are divided into six main parts 👇
+#
+# type() function use to get data types
+#
+# 1 => Numeric : 1.Integer, 2.float, 3.Complex No
+my_integer = 10
+my_float = 3.14
+my_comlex_no = 2 + 5j
+print(type(my_comlex_no))
+
+
+# 2 => Dictionary
+my_dict = {"name": "John", "age": 30, "city": "New York"}
+
+
+# 3 => Boolean : 1.False, 2.True
+my_bool = False
+my_bool = True
+
+
+# 4 => Set
+my_set = {1, 2, 3}
+
+
+# 5 => Sequence Type : 1.String, 2.List, 3.Tuple
+my_string = "Hello, World!"
+my_list = [1, 2, 3, "apple", "banana"]
+my_tuple = (1, 2, 3)
+
+
+# 6 => Special (None)
+x = None
