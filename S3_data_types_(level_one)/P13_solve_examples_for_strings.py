b7236cfb37afb94733f3315ed9e49d6299a22462
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-28 20:48:44 +0330

    A part of content of the thirteenth episode of the third season was added

---
+++ S3_data_types_(level_one)/P13_solve_examples_for_strings.py
@@ -0,0 +1,9 @@
+# Get from user a character and find it count in string
+my_str = "Hello i'm Hassan seyfali pour I love python and 01 world"
+user_character = input("Enter character...")
+print(my_str.count(user_character))
+# ----------------------------------------------------------------------------------------------------------- #
+
+
+# Find the last word of the user's sentence
+user_sentence = input("Enter a sentence...")
