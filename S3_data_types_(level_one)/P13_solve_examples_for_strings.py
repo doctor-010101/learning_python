e2f0b85163e1e0cb1620a843cd95fa6ad3a8b514
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-29 01:56:52 +0330

    The content of the thirteenth episode of the third season was added

--- S3_data_types_(level_one)/P13_solve_examples_for_strings.py
+++ S3_data_types_(level_one)/P13_solve_examples_for_strings.py
@@ -2,8 +2,30 @@
 my_str = "Hello i'm Hassan seyfali pour I love python and 01 world"
 user_character = input("Enter character...")
 print(my_str.count(user_character))
-# ----------------------------------------------------------------------------------------------------------- #
+# # ----------------------------------------------------------------------------------------------------------- #
 
 
 # Find the last word of the user's sentence
 user_sentence = input("Enter a sentence...")
+user_sentence = user_sentence.strip()
+last_space = user_sentence.rfind(" ")
+print(user_sentence[last_space + 1 :])
+# # ----------------------------------------------------------------------------------------------------------- #
+
+
+# Are there words from the second string inside the first string or not?
+string1 = input("Enter str 1...")
+string2 = input("Enter str 2...")
+print(string1 in string2)
+# ----------------------------------------------------------------------------------------------------------- #
+
+
+# Remove tabs and spaces from user input
+user_string = input("Enter string... :) ")
+print(user_string.replace(" ", "").replace("\t", ""))
+# ----------------------------------------------------------------------------------------------------------- #
+
+
+# Remove zero frome user phone number
+user_phone_number = input("Enter your phone number :) ")
+print(user_phone_number.lstrip("0"))
