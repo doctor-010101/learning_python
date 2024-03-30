4656346e253aae17cfdd6394570f156e3384c558
Hassan <hassan.cod.dev.0101@gmail.com>
2024-03-30 20:46:49 +0330

    The content of the 14th and 15th episode of the third season were added

---
+++ S3_data_types_(level_one)/P14,15_solve_exercises_string.py
@@ -0,0 +1,36 @@
+# Exercise 1 => Get sentences, words, characters and letters from user input.
+
+user_input = input("Enter your text...")
+sentences = (
+    user_input.count(".")
+    + user_input.count("!")
+    + user_input.count("?")
+    + user_input.count(";")
+)
+words = user_input.count(" ") + 1
+characters = len(user_input)
+letters = characters - (
+    user_input.count(".")
+    + user_input.count("!")
+    + user_input.count("?")
+    + user_input.count(";")
+    + user_input.count("-")
+    + user_input.count(",")
+    + user_input.count("-")
+    + user_input.count(":")
+    + user_input.count(" ")
+)
+print("Number of sentences : ", sentences)
+print("Number of words : ", words)
+print("Number of characters : ", characters)
+print("Number of letters : ", letters)
+
+
+# Exercise 2 => Get a character form user and print it unicode.
+user_character = input("Enter a character...")
+print(ord(user_character))
+
+
+# Exercise 3 => Is user input number or not?
+user_phone_number = input("Enter yout phone number...")
+print(user_phone_number.isnumeric())
