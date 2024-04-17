e5a0c8a166a64b69709f1094128582b102e1c809
Hassan <hassan.cod.dev.0101@gmail.com>
2024-04-17 19:24:34 +0330

    The content of the third part of the fifth season updated

--- S5_control_commands_(repeat)/P3_break_continue_else.py
+++ S5_control_commands_(repeat)/P3_break_continue_else.py
@@ -1,4 +1,23 @@
-# break => The loop will no longer repeat when it reaches the break command.
+"""
+continue:
+When the "continue" statement is encountered within a "while" loop, the current iteration of the loop is terminated,
+ and the program execution jumps back to the beginning of the loop. In other words,
+ it skips the rest of the code in the loop and starts the next iteration.
+
+else:
+The "else" clause in a "while" loop in Python is executed when the loop condition becomes false.
+ In other words, when the loop naturally terminates due to the condition being false, the code block under the "else" clause is executed.
+
+break:
+The "break" statement is used within a "while" loop to immediately terminate the loop's execution. When the code encounters a "break" statement,
+ the loop is exited, and the program continues execution from the next statement after the loop.
+
+These statements help control the flow of execution within loops and allow you to tailor the behavior of the loop according to your requirements.
+
+"""
+
+
+# break
 # i = 1
 # while i <= 100:
 #     print(i)
@@ -32,7 +51,7 @@
 # print("smallest_number is : ", smallest_number)
 
 
-# continue =>
+# continue
 i = 0
 while i < 10:
     i += 1
@@ -41,7 +60,7 @@ while i < 10:
     print(i)
 
 
-# else =>
+# else
 number = int(input("Enter a number : "))
 i = 2
 if number > 1:
