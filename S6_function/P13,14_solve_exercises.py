2dc236e9f1452d2d0f1149e60593ce2a0b3d75fb
Hassan <hassan.cod.dev.0101@gmail.com>
2024-05-10 22:46:35 +0330

    The content of the 13,14th part of the tenth season updated

---
+++ S6_function/P13,14_solve_exercises.py
@@ -0,0 +1,41 @@
+# 🍀 1 🍀
+# def len_(it):
+#     counter = 0
+#     for _ in it:
+#         counter += 1
+#     return counter
+
+
+# str = "python"
+# l = [1, 3, 4, 5, 6]
+# t = ("a", "b", "c")
+
+# print(len_(str))
+# print(len_(l))
+# print(len_(t))
+
+
+# 🍀 2 🍀
+# def max_(*args):
+#     m = float("-inf")
+#     for i in args:
+#         if i > m:
+#             m = i
+#     return m
+
+
+# print(max_(10, 20, 65, 23, 10, -1))
+
+
+# 🍀 3 🍀
+# def sum_(it):
+#     s = 0
+#     for i in it:
+#         s += i
+#     return s
+
+
+# print(sum_((10, 12, 11)))
+
+
+
