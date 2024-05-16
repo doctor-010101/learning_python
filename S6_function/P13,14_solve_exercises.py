16297c7bf9371a3d1ceb4e07e0f4ec95621427ea
Hassan <hassan.cod.dev.0101@gmail.com>
2024-05-16 19:09:09 +0330

    The content of the 13,14th part of the tenth season updated

--- S6_function/P13,14_solve_exercises.py
+++ S6_function/P13,14_solve_exercises.py
@@ -38,4 +38,44 @@
 # print(sum_((10, 12, 11)))
 
 
+# 🍀 4 🍀
+# def square(n):
+#     for i in range(1, n):
+#         if i ** 2 == n:
+#             print(f"Yes! {i} * {i} = {n}")
+#             break
+#     else:
+#         print("No!")
 
+
+# x = int(input("x: "))
+# square(x)
+
+
+# 🍀 5 🍀
+# def discount(rate, price):
+#     discount_rate = int(price * rate / 100)
+#     new_price = price - discount_rate
+#     print("discount_rate :", discount_rate)
+#     print("new-price :", new_price)
+
+
+# rate = int(input("rate: "))
+# price = int(input("price: "))
+# discount(rate, price)
+
+
+# 🍀 6 🍀
+def func(character):
+    if 48 <= ord(character) <= 57:
+        print("The character is number!")
+    elif 65 <= ord(character) <= 90:
+        print("The character is  uppercase letter!")
+    elif 97 <= ord(character) <= 122:
+        print("The character is lowercase letter!")
+    else:
+        print("Other!")
+
+
+character = input("Enter a character : ")
+func(character)
