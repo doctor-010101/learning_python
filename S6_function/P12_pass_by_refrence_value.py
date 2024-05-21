03977499220c5d3aed6718f7470c69c81cb8fb6b
Hassan <hassan.cod.dev.0101@gmail.com>
2024-05-21 22:54:29 +0330

    The content of the 12th part of the tenth season updated

--- S6_function/P12_pass_by_refrence_value.py
+++ S6_function/P12_pass_by_refrence_value.py
@@ -1,2 +1,26 @@
-# pass by value
-# pass by refrence
+# In the Python programming language, "pass by reference" and "pass by value" are two different methods for passing data to functions. In Python, objects are passed by reference, but these references are copied by value. Therefore, sometimes this method is referred to as "pass by value of the reference." Here is an example to explain further:
+
+def append_to_list(element, list=[]):
+    list.append(element)
+    return list
+
+my_list = append_to_list(12)
+print(my_list)  # Output: [12]
+
+my_second_list = append_to_list(17)
+print(my_second_list)  # Output: [12, 17]
+# In the above example, list is a list object that is passed to the function append_to_list. Since lists in Python are mutable, changes made to the list within the function are also visible outside of the function. This demonstrates "pass by reference" behavior.
+
+# Now, if we pass an immutable object like an integer or a string to a function, changes made to the variable within the function will not affect the variable outside of the function:
+
+def modify_number(number):
+    number += 10
+    return number
+
+original_number = 5
+new_number = modify_number(original_number)
+print(original_number)  # Output: 5
+print(new_number)  # Output: 15
+# In this example, original_number does not change because an integer is an immutable object, and changes within the function modify_number only apply to a local copy of the variable. This behavior is similar to "pass by value."
+
+# Therefore, in Python, passing mutable objects to functions can result in changes that are visible outside of the function, while passing immutable objects does not change the variables outside of the function. These differences should be considered when writing code in Python to prevent mistakes.
