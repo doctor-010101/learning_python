40892047fdaca2b8fa295518056becddab6d743d
Hassan <hassan.cod.dev.0101@gmail.com>
2024-05-22 23:08:05 +0330

    The content of the 20th part of the sixth season updated

--- S6_function/P20_decorator_2.py
+++ S6_function/P20_decorator_2.py
@@ -1,1 +1,40 @@
-# wrapper
+import functools
+
+
+
+def plus(func):
+    @functools.wraps(func)
+    def inner(*x, **y):
+        print("+" * 20)
+        func(*x, **y)
+        print("+" * 20)
+    return inner
+
+
+def exclamation(func):
+    @functools.wraps(func)
+    def inner(*x, **y):
+        print("!" * 20)
+        func(*x, **y)
+        print("!" * 20)
+    return inner
+
+
+@plus
+@exclamation
+def message(msg):
+    print("I love", msg)
+
+
+message("py")
+
+
+# The decorator formula
+def decorator(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        # Do something before
+        value = func(*args, **kwargs)
+        # Do something after
+        return value
+    return wrapper_decorator
