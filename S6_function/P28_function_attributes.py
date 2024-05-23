01e91a3e72e08e6d82a8760c60e5a5b6c44c2865
Hassan <hassan.cod.dev.0101@gmail.com>
2024-05-24 01:45:19 +0330

    The content of the 28th part of the sixth season added

---
+++ S6_function/P28_function_attributes.py
@@ -0,0 +1,7 @@
+# def.attr_name = "new attr"
+# setattr(function, "name", "value")
+# __dict__
+# getattr(object, "attribute", "default")
+# hasattr(object, attribute)
+# delattr(object, attribute)
+# del def.attr
